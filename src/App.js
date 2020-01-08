5563755be9fea02e75eea32be70824b75648766e
Wyatt Phillips <datscigye@gmail.com>
2020-01-07 19:30:06 -0500

    Add contact component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header branding={"Contact Manager"} />
+        <Contact
+          name={"Timmy Turner"}
+          email={"tturner@fmail.wand"}
+          phone={"622-543-7789"}
+        />
+        <Contact
+          name={"Denzel Crocker"}
+          email={"fairiesarereal@tcn.net"}
+          phone={"622-543-6666"}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
